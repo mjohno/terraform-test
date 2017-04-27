2ab9faa38457c97e70475c3a3f2e54e3538e7909
mjohno <mattgjo@gmail.com>
2017-04-28 01:08:38 +1000

    Successful opsworks stack build

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ https://s3.amazonaws.com/opsworks-demo-assets/opsworks-linux-demo-cookbooks-node
 
 * Any IP range -> Chose 172.32.0.0/24. Seems to be default, lots of room to scale.
 * Created 2 subnets -> 172.32.1.0/16 and 172.32.0.0/16. Uses seperate AZ's by default.
+
+
+## Gotcha's
+
+* Hardcoded arn for `aws_iam_role` `aws_iam_instance_profile` which are required variables to build an opsworks stack since I did not have permissions to read it.
+* Default settings for an opswork stack is Chef 11 and Ubuntu 12.04. Needed to change this.
--- test-stack.tf
+++ test-stack.tf
@@ -8,21 +8,30 @@ variable "route53_zone_id" { default = "Z3BRSPUKGCDDFR" }
 # Relies on having '.aws/credentials' available relative to the terraform call. 
 provider "aws" {
   shared_credentials_file = ".aws/credentials"
-  region = "${var.aws_region}"
+  region                  = "${var.aws_region}"
 }
 
-resource "aws_vpc" "mj-trial" {
+resource "aws_vpc" "test" {
   cidr_block = "172.32.0.0/16"
 }
 
-resource "aws_subnet" "mj-trial-a" {
-  vpc_id     = "${aws_vpc.mj-trial.id}"
+resource "aws_subnet" "test_a" {
+  vpc_id            = "${aws_vpc.test.id}"
   availability_zone = "${var.aws_region}a"
-  cidr_block = "172.32.0.0/24"
+  cidr_block        = "172.32.0.0/24"
 }
 
-resource "aws_subnet" "mj-trial-b" {
-  vpc_id     = "${aws_vpc.mj-trial.id}"
+resource "aws_subnet" "test_b" {
+  vpc_id            = "${aws_vpc.test.id}"
   availability_zone = "${var.aws_region}b"
-  cidr_block = "172.32.1.0/24"
+  cidr_block        = "172.32.1.0/24"
+}
+
+resource "aws_opsworks_stack" "test" {
+  name                         = "mj-test-stack"
+  region                       = "${var.aws_region}"
+  service_role_arn             = "arn:aws:iam::623134608261:role/aws-opsworks-service-role"
+  default_instance_profile_arn = "arn:aws:iam::623134608261:instance-profile/aws-opsworks-ec2-role"
+  vpc_id                       = "${aws_vpc.test.id}"
+  default_subnet_id            = "${aws_subnet.test_a.id}"
 }
