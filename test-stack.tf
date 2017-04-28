c7d917ed43ca10be8299adb68ee7774b0b264f32
mjohno <mattgjo@gmail.com>
2017-04-28 19:32:05 +1000

    Creating internet gateway and routes for ELB

--- test-stack.tf
+++ test-stack.tf
@@ -11,6 +11,8 @@ provider "aws" {
   region                  = "${var.aws_region}"
 }
 
+########### VPC ###########
+
 resource "aws_vpc" "test" {
   cidr_block = "172.32.0.0/16"
 }
@@ -26,6 +28,28 @@ resource "aws_subnet" "test_b" {
   availability_zone = "${var.aws_region}b"
   cidr_block        = "172.32.1.0/24"
 }
+
+resource "aws_route_table_association" "test_a" {
+  subnet_id = "${aws_subnet.test_a.id}"
+  route_table_id = "${aws_vpc.test.main_route_table_id}"
+}
+
+resource "aws_route_table_association" "test_b" {
+  subnet_id = "${aws_subnet.test_b.id}"
+  route_table_id = "${aws_vpc.test.main_route_table_id}"
+}
+
+resource "aws_internet_gateway" "test_gateway" {
+  vpc_id = "${aws_vpc.test.id}"
+}
+
+resource "aws_route" "internet_gateway_route" {
+  route_table_id = "${aws_vpc.test.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.test_gateway.id}"
+}
+
+########### ELB ###########
 
 resource "aws_security_group" "elb" {
   name = "mj-elb-sg"
@@ -56,12 +80,6 @@ resource "aws_elb" "test" {
   subnets            = ["${aws_subnet.test_a.id}", "${aws_subnet.test_b.id}"]
   security_groups    = ["${aws_security_group.elb.id}"]
 
-  access_logs {
-    bucket        = "foo"
-    bucket_prefix = "bar"
-    interval      = 60
-  }
-
   listener {
     instance_port     = "${var.instance_port}"
     instance_protocol = "http"
@@ -77,6 +95,8 @@ resource "aws_elb" "test" {
     interval            = 30
   }
 }
+
+########### Opsworks ###########
 
 resource "aws_opsworks_stack" "test" {
   name                          = "mj-test-stack"
