9b4a720167d9773d25b241d53814cf06d7f2603c
mjohno <mattgjo@gmail.com>
2017-04-28 13:47:26 +0200

    Adding deploy scripts to opsworks layer

--- opsworks.tf
+++ opsworks.tf
@@ -7,12 +7,19 @@ resource "aws_opsworks_stack" "test" {
   default_subnet_id             = "${aws_subnet.test_a.id}"
   configuration_manager_version = "12"
   default_os                    = "Ubuntu 16.04 LTS"
+  use_custom_cookbooks          = "true"
+  default_root_device_type      = "ebs"
+  custom_cookbooks_source       = {
+    type = "archive"
+    url  = "https://s3.amazonaws.com/opsworks-demo-assets/opsworks-linux-demo-cookbooks-nodejs.tar.gz"
+  }
 }
 
 resource "aws_opsworks_custom_layer" "test" {
-  name                  = "Matt's Test Layer"
-  short_name            = "mjt"
-  stack_id              = "${aws_opsworks_stack.test.id}"
-  elastic_load_balancer = "${aws_elb.test.name}"
+  name                      = "Matt's Test Layer"
+  short_name                = "mjt"
+  stack_id                  = "${aws_opsworks_stack.test.id}"
+  elastic_load_balancer     = "${aws_elb.test.name}"
   custom_security_group_ids = ["${aws_security_group.web.id}"]
+  custom_deploy_recipes     = ["nodejs_demo"]
 }
